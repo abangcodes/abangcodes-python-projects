a66775471064c5910d77673fe83841d55a573648
Kevin Abangco <148144001+abangcodes@users.noreply.github.com>
2023-11-03 14:58:59 +0100

    Add files via upload

---
+++ asciiinitals.py
@@ -0,0 +1,31 @@
+#My initials are KBA
+
+name = """
+K   K BBBB    A
+K  K  B   B  A A
+K K   B   B A   A
+KK    BBBB  AAAAA
+K K   B   B A   A
+K  K  B   B A   A
+K   K BBBB  A   A
+"""
+
+print (name)
+
+print("      *     *                    ")
+print("                      *          ")
+print("*         *                 *    ")
+print("     *           *     *         ")
+print("           HHHHHHH        *      ")
+print(" *         HHHHHHH               ")
+print("     *   HHHHHHHHHHH    *     *  ")
+print("         *   @ @   *             ")
+print("        *     ^     *            ")
+print("  *       *       *         *    ")
+print("         SSS * * S               ")
+print("         * SSSSSSSS *            ")
+print("       *      O   SS  *          ")
+print("      *       O    S   *         ")
+print("      *       O        *         ")
+print("       *              *          ")
+print("           *   *   *             ")
